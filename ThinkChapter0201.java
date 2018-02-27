0fb740f9ebd88936b5984a513c3c8a24f7adf441
ti17m006 <ti17m006@technikum-wien.at>
2018-02-27 11:37:09 +0100

    Start

---
+++ ThinkChapter0201.java
@@ -0,0 +1,16 @@
+
+/*
+ * 
+ * Create a new program named ThinkChapter0201.java . 
+ * Copy or type in something like the hello world program 
+ * and make sure you can compile and run it, push it to git repo.
+ * 
+ */
+
+class ThinkChapter0201 {
+	
+	public static void main(String [] args) {
+		
+		System.out.print("Hello :D\n");
+	}
+}
