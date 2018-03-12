2050d4a72aa753ebe8e9eea0a8c7b314bfd5be28
Damjan <ti17m006@technikum-wien.at>
2018-03-12 15:13:05 +0100

    Convert seconds

---
+++ ThinkChapter0307.java
@@ -0,0 +1,26 @@
+
+/*
+    Program that converts a total number 
+    of seconds to hours, minutes, and seconds.
+*/
+
+import java.util.Scanner;
+
+class ThinkChapter0307 {
+    public static void main(String[] args) {
+
+        Scanner myIn = new Scanner(System.in);
+
+        int inSeconds;
+        int hour, minute, second;
+
+        System.out.print("Enter seconds: ");
+        inSeconds = myIn.nextInt();
+
+        hour = (inSeconds / 60) / 60;
+        minute = (inSeconds / 60) % 60;
+        second = (inSeconds % 60);
+
+        System.out.println("hours: " + hour + " minutes: " + minute + " seconds: " + second);
+    }
+}
