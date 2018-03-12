1d1b247f15215a3010c3715e42f2d64d2c10d171
Damjan <ti17m006@technikum-wien.at>
2018-03-12 15:00:37 +0100

    Convert temperature from Celsius to Feirenheit

---
+++ ThinkChapter0306.java
@@ -0,0 +1,26 @@
+
+/* 
+    Convert temperature from Celsius to Feirenheit
+    f = c x (9/5) + 32
+*/
+
+import java.util.Scanner;
+
+class ThinkChapter0306 {
+    public static void main(String[] args) {
+
+        Scanner myIn = new Scanner(System.in);
+
+        // Do not integer division!
+        final double CONST_01 = (double) 9 / (double) 5;
+        final double CONST_02 = 32;
+
+        double celsius;
+        double feirenheit;
+
+        System.out.print("Enter temperature in celsious: ");
+        celsius = myIn.nextDouble();
+        feirenheit = (celsius * CONST_01) + CONST_02;
+        System.out.println("Given temperature in celsius is " + feirenheit + " in feirenheit");
+    }
+}
