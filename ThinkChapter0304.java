e261764043719a6403522a0f3561b51f46d62a4c
Damjan <ti17m006@technikum-wien.at>
2018-03-12 13:26:51 +0100

    Convert centimetes to inches using type cast

---
+++ ThinkChapter0304.java
@@ -0,0 +1,22 @@
+/*
+    Convert centimetes to inches using type cast
+    
+*/
+
+import java.util.Scanner;
+
+class ThinkChapter0304 {
+    public static void main(String[] args) {
+        
+        final double CM_PER_INCH = 2.54;
+
+        int cm;
+        double inch;
+        Scanner myInput = new Scanner(System.in);
+
+        System.out.print("Enter centimeters: ");
+        cm = myInput.nextInt();
+        inch = (double) (cm) / CM_PER_INCH;
+        System.out.println( cm + " centimeters are " + inch + " inches");
+    }
+}
