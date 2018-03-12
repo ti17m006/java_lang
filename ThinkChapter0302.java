ac5ce3d5162e31f13165c78e24dbe439d5b70bb5
Damjan <ti17m006@technikum-wien.at>
2018-03-12 12:42:39 +0100

    Convert inches to centimeters

---
+++ ThinkChapter0302.java
@@ -0,0 +1,19 @@
+/* 
+    Convert inches to centimeters
+ */
+
+import java.util.Scanner;
+
+class ThinkChapter0302 {
+    public static void main(String [] args) {
+        
+        int inch;
+        double cm;
+        Scanner myInput = new Scanner(System.in);
+        
+        System.out.print("Enter inches: ");
+        inch = myInput.nextInt();
+        cm = inch * 2.54;
+        System.out.println(inch + " inches = " + cm + " centimeters");
+    }
+}
