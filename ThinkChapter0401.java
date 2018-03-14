2ffa175c18fb0fc9bd36b05e3896f682b7aae934
ti17m006 <ti17m006@technikum-wien.at>
2018-03-14 11:59:39 +0100

    Static methods

---
+++ ThinkChapter0401.java
@@ -0,0 +1,59 @@
+/* 
+	
+*/
+
+import java.util.Scanner;
+
+// sqrt, sin, cos, tan
+import java.lang.Math;
+
+class ThinkChapter0401 {
+	
+	public static void newLine() {
+		System.out.println();
+	}
+	
+	public static void printTwice(String s) {
+		System.out.println(s);
+		System.out.println(s);
+	}
+	
+	public static void printTime(int hour, int minute) {
+		System.out.print(hour);
+		System.out.print(":");
+		System.out.print(minute);
+		newLine();
+	}
+
+	public static void add(int a) {
+		int b = 5;
+		int c = a + b;
+		System.out.print("c = " + c);
+		newLine();
+	}
+
+	public static void baffle() {
+		System.out.print("wug");
+		ping();
+	}
+	
+	public static void ping() {
+		System.out.println(".");
+		// baffle();
+	}
+
+	public static void main(String [] args) {
+		System.out.println("First line.");
+		newLine();
+		System.out.println("Second line.");
+		printTime(11, 22);
+		printTime(11 + 1, 10 + 12);
+
+		int a = 2;
+		int b = 9;
+		add(3);
+		add(a);
+		add(b);
+		baffle();
+	}
+}
