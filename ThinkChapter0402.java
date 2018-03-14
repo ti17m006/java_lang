48ddd9acf32b1bcb0744a29108d50da9c11b02b7
ti17m006 <ti17m006@technikum-wien.at>
2018-03-14 12:00:22 +0100

    Methods print us and european date format

---
+++ ThinkChapter0402.java
@@ -0,0 +1,30 @@
+/*
+    1. Write a method called printAmerican that takes the day, date, month and year as parameters and that displays them in American format.
+
+    2. Test your method by invoking it from main and passing appropriate arguments. The output should look something like this (except that the date might be different):
+        Saturday, July 22, 2015
+    
+    3. Once you have debugged printAmerican,
+    write another method called printEuropean that 
+    displays the date in European format.
+*/
+
+import java.util.Scanner;
+
+class ThinkChapter0402 {
+
+    public static void printAmerican(String day, int date, String month, int year) {
+        String format = day + " " + month + " " + day + " " + year;
+        System.out.println(format);
+    }
+
+    public static void printEuropean(String day, int date, String month, int year) {
+        String format = day + " " + date + " " + month + " " + year;
+        System.out.println(format);
+    }
+
+    public static void main(String[] args) {
+        printAmerican("Saturday", 22, "July", 2015);
+        printEuropean("Saturday", 22, "July", 2015);        
+    }
+}
