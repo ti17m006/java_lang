4bc066372bdf6c7f4286d938595eeaaac7836739
ti17m006 <ti17m006@technikum-wien.at>
2018-03-15 13:40:34 +0100

    Conditional statements first example

---
+++ ThinkChapter0502.java
@@ -0,0 +1,23 @@
+/**
+ * ThinkChapter0502
+ * Conditional statements
+ * Check if the number is positive
+ */
+public class ThinkChapter0502 {
+
+    private static void positive03(int a) {
+        
+    }
+
+    private static void positive02(int a) {
+        
+    }
+
+    private static void positive01(int a) {
+        
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
