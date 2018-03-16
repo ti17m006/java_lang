b7e784ba88f60133daa5a534ddac5bfd67fb2b06
ti17m006 <ti17m006@technikum-wien.at>
2018-03-16 17:55:59 +0100

    methods introduction / public static void

--- ThinkChapter0402.java
+++ ThinkChapter0402.java
@@ -1,12 +1,16 @@
-/*
-    1. Write a method called printAmerican that takes the day, date, month and year as parameters and that displays them in American format.
 
-    2. Test your method by invoking it from main and passing appropriate arguments. The output should look something like this (except that the date might be different):
-        Saturday, July 22, 2015
-    
-    3. Once you have debugged printAmerican,
-    write another method called printEuropean that 
-    displays the date in European format.
+/*
+*    1. Write a method called printAmerican that takes the day, date, 
+*    month and year as parameters and that displays them in American format.
+*
+*    2. Test your method by invoking it from main and passing appropriate
+*    arguments. The output should look something like this (except that 
+*    the date might be different):
+*        Saturday, July 22, 2015
+*    
+*    3. Once you have debugged printAmerican,
+*    write another method called printEuropean that 
+*    displays the date in European format.
 */
 
 import java.util.Scanner;
@@ -25,6 +29,6 @@ class ThinkChapter0402 {
 
     public static void main(String[] args) {
         printAmerican("Saturday", 22, "July", 2015);
-        printEuropean("Saturday", 22, "July", 2015);        
+        printEuropean("Saturday", 22, "July", 2015);
     }
 }
