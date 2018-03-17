5f35d33298f90a6e49d945d91dfc0039674bc82d
ti17m006 <ti17m006@technikum-wien.at>
2018-03-17 19:27:35 +0100

    While loop - Print rows

--- ThinkChapter0703.java
+++ ThinkChapter0703.java
@@ -1,3 +1,4 @@
+
 /**
  * ThinkChapter0703
  * Print table
@@ -6,28 +7,42 @@ import java.util.Scanner;
 
 class ThinkChapter0703 {
 
-    public static void tmp(int start, int i, int n) {
+    public static void print(int offset, int i, int n) {
         while (i <= n) {
             System.out.print(i + " ");
-            i = start + i;
-        } 
+            i = offset + i;
+        }
     }
 
-    public static void printRow(int n, int m) {
-        int i = n / m;
-        int start = i;
-        if (i % 2 != 0) {
-            tmp(start, i, n);  
+    public static void printRow(int m, int n) {
+        int i = m / n;
+        int offset = i;
+        print(offset, i, m);
+        System.out.println();
+    }
+
+    public static void printRowUp(int m, int n) {
+        int i = m / n;
+        int offset = i;
+        if (i % 2 == 0) {
+            print(offset, i, m - (i * i) + i);
         } else {
-            tmp(start, i, n);
+            print(offset, i, m - (i * i) + i);
         }
         System.out.println();
+    }
+
+    public static void printRowDown(int m, int n) {
+        int i = m / n;
+        // int offset = i;
+        printRow(i * i, i);
+        // print(offset, i, i * i); System.out.println();
     }
 
     public static void printTable(int n) {
         int i = 1;
         while (i <= n) {
-            printRow(i * n, n);
+            printRowUp(i * n, n);
             i = i + 1;
         }
         System.out.println();
@@ -36,7 +51,7 @@ class ThinkChapter0703 {
     public static void run() {
         int number;
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter number: " );
+        System.out.print("Enter number: ");
         number = in.nextInt();
         printTable(number);
     }
